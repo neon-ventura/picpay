90dc08289d02e28f5394e66c2249b6bc3fbf0917
Neon Ventura <neonventura@gmail.com>
2025-02-17 13:26:44 -0300

    new chalenge [PICPAY API]

---
+++ src/main/java/com/picpay/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.picpay.repositories;
+
+import com.picpay.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+}
